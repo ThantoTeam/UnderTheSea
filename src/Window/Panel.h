6f5ccd475a4c0e8533a36029d727a20caa4b613e
AntoninLoubiere <antonin.loubiere@laposte.net>
2018-11-07 19:40:53 +0100

    Add panel class

---
+++ src/Window/Panel.h
@@ -0,0 +1,22 @@
+/*
+ * Panel.h
+ *
+ *  Created on: 7 nov. 2018
+ *      Author: antonin
+ */
+
+#ifndef SRC_WINDOW_PANEL_H_
+#define SRC_WINDOW_PANEL_H_
+
+#include "Widget/Widget.h"
+
+namespace ta {
+
+class Panel: public Widget {
+public:
+	virtual void setup() = 0; // just add setup methode
+};
+
+} /* namespace ta */
+
+#endif /* SRC_WINDOW_PANEL_H_ */
