99de7c09f1d561c8390f50eddd2aeccb60e3aaae
AntoninLoubiere <antonin.loubiere@laposte.net>
2018-12-28 18:08:36 +0100

    Add map method
    Add a map methods in geneal file. This method tranform a number with min
    and max to a another number with min and max

--- src/General.h
+++ src/General.h
@@ -25,6 +25,19 @@ namespace general
 std::string getCurrentStringTime();
 
 bool testFileExist(std::string filePath);
+
+template<typename T>
+/* return a map of a number
+ * \param var: the var to convert
+ * \param varMin: the min of var
+ * \param varMax: the max of var
+ * \param outputMin: the min of output
+ * \param outputMax: the max of output */
+T map(T var, T varMin, T varMax, T outputMin, T outputMax)
+{
+    return (T) ((var - varMin) / (double) (varMax - varMin)
+            * (outputMax - outputMin) + outputMin);
+}
 }  // namespace general
 
 
